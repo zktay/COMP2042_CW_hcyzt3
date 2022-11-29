79a8335f85958a52429b56e22e3b24db76ce570d
ZK_Tay <67721858+zktay@users.noreply.github.com>
2022-11-30 00:25:28 +0800

    merging to Desktop
    Take a rest

--- src/main/java/com/example/demo/leaderboard.java
+++ src/main/java/com/example/demo/leaderboard.java
@@ -10,10 +10,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.skin.TableHeaderRow;
 import javafx.scene.layout.Background;
@@ -28,16 +25,16 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.net.URL;
-import java.util.Arrays;
-import java.util.Objects;
-import java.util.ResourceBundle;
+import java.util.*;
 
 public class leaderboard implements Initializable {
     private Parent root;
     private Stage stage;
     public static Color colorSelected = Color.rgb(189,177,92);
+    //ArrayList<String> podiumArray = new ArrayList<String>();
+    String[][] podiumArray;
     @FXML
-    private TableView<Scoring> leaderboard;
+    private TableView leaderboard;
     @FXML
     private BorderPane Pane;
     ObservableList<Scoring> Scoring;
@@ -62,19 +59,23 @@ public class leaderboard implements Initializable {
     @FXML
     private MenuItem tutorial;
     @FXML
-    private TableColumn<leaderboard, Integer> index;
+    private TableColumn  index;
     @FXML
-    private TableColumn<leaderboard, String> difficulty;
+    private TableColumn difficulty;
     @FXML
-    private TableColumn<leaderboard, String> result;
+    private TableColumn result;
+    @FXML
+    private TableColumn score;
+    @FXML
+    private TableColumn username;
 
     @FXML
-    private TableColumn<leaderboard, String> score;
+    private TableColumn variant;
     @FXML
-    private TableColumn<leaderboard, String> username;
+    private TextField textBox;
 
     @FXML
-    private TableColumn<leaderboard, String> variant;
+    private Button searchButton;
     @FXML
     void TutorialButton(ActionEvent event) {
 
@@ -103,7 +104,7 @@ public class leaderboard implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
         Pane.setBackground(new Background(new BackgroundFill(Main.colorSelected, null, null)));
         getScore();
-        leaderboard.setItems(Scoring);
+        podium();
         index.setCellValueFactory(new PropertyValueFactory<>("Index"));
         username.setCellValueFactory(new PropertyValueFactory<>("Username"));
         score.setCellValueFactory(new PropertyValueFactory<>("Score"));
@@ -133,12 +134,126 @@ public class leaderboard implements Initializable {
                 for (String temp : arrayLine){
                     for (int i = 0; i < arrayLine.length; i++) {
                         String[] data = temp.split(", ");
-
                             Scoring.add(new Scoring(counter, data[0], Integer.parseInt(data[1]), data[2], data[3], data[4]));
                     }
                 }
                 counter ++;
             }
+
+            leaderboard.setItems(Scoring);
+            //leaderboard.getSortOrder().add(TableColumn<Scoring, score>);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    void searchAction(ActionEvent event) {
+        //clear the observableList to remove all previous items
+        Scoring.clear();
+        try (BufferedReader br = new BufferedReader(new FileReader(new File("data/score.txt")))) {
+            String line;
+            int counter = 1;
+            while ((line = br.readLine()) != null){
+                String[] arrayLine = line.split(";");
+                for (String temp : arrayLine){
+                    for (int i = 0; i < arrayLine.length; i++) {
+                        String[] data = temp.split(", ");
+                        //Compare the search value and compare with the text file, if match then add the respective to the Observable list.
+                        if (data[0].equals(textBox.getText())) {
+                            Scoring.add(new Scoring(counter, data[0], Integer.parseInt(data[1]), data[2], data[3], data[4]));
+                            leaderboard.setItems(Scoring);
+                            //resetting the index
+                            counter ++;
+                        } else if (data[1].equals(textBox.getText())) {
+                            Scoring.add(new Scoring(counter, data[0], Integer.parseInt(data[1]), data[2], data[3], data[4]));
+                            leaderboard.setItems(Scoring);
+                            counter ++;
+                        } else if (data[2].equals(textBox.getText())) {
+                            Scoring.add(new Scoring(counter, data[0], Integer.parseInt(data[1]), data[2], data[3], data[4]));
+                            leaderboard.setItems(Scoring);
+                            counter ++;
+                        } else if (data[3].equals(textBox.getText())) {
+                            Scoring.add(new Scoring(counter, data[0], Integer.parseInt(data[1]), data[2], data[3], data[4]));
+                            leaderboard.setItems(Scoring);
+                            counter ++;
+                        }else if (data[4].equals(textBox.getText())) {
+                            Scoring.add(new Scoring(counter, data[0], Integer.parseInt(data[1]), data[2], data[3], data[4]));
+                            leaderboard.setItems(Scoring);
+                            counter ++;
+
+                        }
+
+                    }
+
+
+                }
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static String[][] bubbleSort(String arr[][]) {
+        int n = arr.length;
+        int tempAge = 0;
+        String tempName = "";
+        int []result = new int[n];
+        String myName[] = new String [n];
+        for (int i = 0; i < n; i++)
+        {
+            result[i] = Integer.parseInt(arr[i][1]);
+            myName[i] = arr[i][0];
+        }
+        boolean sorted = false;
+        while (!sorted){
+            sorted = true;
+            for(int i = 1; i < n; i++) {
+                if(result[i-1] > result[i]) {
+                    tempAge = result[i-1];
+                    result[i-1] = result[i];
+                    result[i] = tempAge;
+                    tempName = myName[i-1];
+                    myName[i-1] = myName[i];
+                    myName[i] = tempName;
+                    sorted = false;
+                }
+            }
+        }
+        for (int i = 0; i < arr.length; i++)
+        {
+            arr[i][0] = myName[i];
+            arr[i][1] = Integer.toString(result[i]);
+        }
+        return arr;
+    }
+
+    void podium(){
+        try (BufferedReader br = new BufferedReader(new FileReader(new File("data/score.txt")))) {
+            String line;
+            while ((line = br.readLine()) != null){
+                String[] arrayLine = line.split(";");
+                for (String temp : arrayLine){
+                    for (int i = 0; i < arrayLine.length; i++) {
+                        String[] temp1 = temp.split(", ");
+                        podiumArray = new String[Integer.parseInt(temp1[0])][Integer.parseInt(temp1[1])];
+                        //podiumArray = new String[]{Arrays.toString(podiumArray)};
+                        System.out.println(Arrays.toString(podiumArray));
+                        System.out.println(Arrays.deepToString(podiumArray));
+                        bubbleSort(podiumArray);
+                        //https://stackoverflow.com/questions/60841514/2d-bubble-sort-java-program-that-have-string-and-integer-in-array
+                        //podiumArray.add();
+
+                        //String data = temp1[0] + ", " +temp1[1];
+                        //podiumArray.add(data);
+                    }
+                }
+            }
+
+
 
         } catch (IOException e) {
             e.printStackTrace();
